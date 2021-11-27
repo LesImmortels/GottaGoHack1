b347919cc6e93c9b10e865931f606c2b08008162
Ivan Imbert <ivan.imbert@epita.fr>
2021-11-27 08:21:31 +0100

    Update App.js

--- restock/src/App.js
+++ restock/src/App.js
@@ -1,35 +1,36 @@
 import './App.css';
-import {Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Sidebar from "./components/Sidebar";
 import Orders from "./pages/Orders";
 import Stocks from "./pages/Stocks";
-import {useDispatch, useSelector} from "react-redux";
-import {useEffect} from "react";
-import {checkUserSession} from "./redux/User/user.actions";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { checkUserSession } from "./redux/User/user.actions";
+
 
 const mapState = (state) => ({
-    currentUser: state.user.currentUser,
+  currentUser: state.user.currentUser,
 });
 
 function App(props) {
 
-    const dispatch = useDispatch();
-    const { currentUser } = useSelector(mapState);
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector(mapState);
 
-    useEffect(() => {
-        dispatch(checkUserSession());
-    }, []);
+  useEffect(() => {
+    dispatch(checkUserSession());
+  }, []);
   return (
     <div className="flex flex-row h-screen">
       {/* Side bar here */}
-        <Sidebar/>
+      <Sidebar />
       <div className="w-full">
         <Routes>
           {/* <Route exact path="/" component={Home}/> */}
-            <Route exact path="/stock" element={<Stocks/>}/>
-            <Route exact path="/orders" element={<Orders/>}/>
-          <Route exact path="/" element={<Dashboard/>} />
+          <Route exact path="/stock" element={<Stocks />} />
+          <Route exact path="/orders" element={<Orders />} />
+          <Route exact path="/" element={<Dashboard />} />
         </Routes>
       </div>
     </div>
