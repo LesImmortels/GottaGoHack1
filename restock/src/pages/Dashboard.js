2b35d329c44896a7099e6d7434ee8ed22ddd5f97
raphael.montes <raphael.montes@epita.fr>
2021-11-27 20:13:16 +0100

    Prediction

--- restock/src/pages/Dashboard.js
+++ restock/src/pages/Dashboard.js
@@ -1,57 +1,103 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { checkUserSession } from "../redux/User/user.actions";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 import firebaseInstance from "../services/firebase";
 
 const mapState = (state) => ({
-    currentUser: state.user.currentUser,
+  currentUser: state.user.currentUser,
 });
 
-const renderCustomAxisTick = ({ x, y, payload }) => {
-    console.log(payload);
+const createData = (data) => {
+  if (data.length == 0)
+      return [{ "money saved": 0, "surplus": 0, "offer": 0, "demand": 0, "date": "xx-xx-xxxx","money spent": 0 }];
+  if (data.length == 1)
+      return [{ "money saved": 0, "surplus": data[0].offer - data[0].demand, "offer": data[0].offer, "demand": data[0].demand, "date": data[0].date , "money spent": data[0].spent}];
 
-    return (
-        <text>Hello</text>
-    );
-};
+  const res = [];
+  let avg_demand = data[0].demand;
+  for (let i = 1; i < data.length; i++) {
+      res[i - 1] = {
+          "money saved": data[i - 1].spent - data[i].spent,
+          "surplus": data[i].offer - data[i].demand,
+          "date": data[i].date,
+          "offer": data[i].offer,
+          "demand": data[i].demand,
+          "money spent": data[i].spent
+      };
+      avg_demand += data[i].demand;
+  }
+  avg_demand /= data.length;
+  const unit_price = data[data.length - 1].spent / data[data.length - 1].offer;
+  const offer = Math.round((data[data.length - 1].offer + avg_demand) / 2);
+  res[data.length - 1] = {
+      "money saved": Math.round(data[data.length - 1].spent - offer * unit_price),
+      "money spent": Math.round(offer * unit_price),
+      "surplus": offer - avg_demand,
+      "offer": offer,
+      "demand": avg_demand,
+      "date": "next week"
+  };
+  return res;
+}
 
 function Dashboard() {
-    const dispatch = useDispatch();
-    const { currentUser } = useSelector(mapState);
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector(mapState);
 
-    const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        let data = firebaseInstance.getUserDashboard(currentUser.id);
-        data.then((res) => setData(res.data().data))
-    }, [])
-    return (
-        <><h1 class="text-center mt-24 mb-2 font-bold underline text-lg">I like trains</h1><div class="flex flex-row items-center  justify-center shadow rounded-xl m-24 mt-0 p-2 bg-gray-50 h-4/6">
+  useEffect(() => {
+    let data = firebaseInstance.getUserDashboard(currentUser.id);
+    data.then((res) => {
+      if (typeof res.data() != "undefined") {
+        setData(createData(res.data().data));
+      }
+    });
+  }, []);
+  return (
+    <>
+      <div className="w-full h-full px-24">
+        <h1 className="font-black  text-3xl">Dashboard</h1>
+        <h1 class=" mt-24 mb-2 font-bold text-lg">Predicting probable demands</h1>
+        <div class="flex flex-row items-center  justify-center  rounded-xl mt-0 p-2 bg-green-50 h-4/6">
+          {data && (
             <ResponsiveContainer width="75%" height="75%">
-                <LineChart
-                    width={500}
-                    height={300}
-                    data={data}
-                    margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="date" tick={renderCustomAxisTick} />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
+              <LineChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                  top: 5,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="date" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="offer" stroke="#19C087" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="demand" stroke="#F27A35" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="surplus" stroke="#82ca9d" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="money spent" stroke="#00fafa" activeDot={{ r: 8 }}/>
                     <Line type="monotone" dataKey="money saved" stroke="#8884d8" activeDot={{ r: 8 }} />
-                    <Line type="monotone" dataKey="waste" stroke="#82ca9d" />
-                    <Line type="monotone" dataKey="total costs evolution" stroke="#00fafa" />
-                </LineChart>
+              </LineChart>
             </ResponsiveContainer>
-        </div></>
-    )
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Dashboard;
