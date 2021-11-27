847020ada53d4dc75248aef1a8951da8baa12a12
Adam <adam.alani@epita.fr>
2021-11-27 02:18:38 +0100

    basic firebase functions and basic router

--- restock/src/services/firebase.js
+++ restock/src/services/firebase.js
@@ -11,5 +11,29 @@ class Firebase {
             appId: "1:194493879759:web:9aba5454746c61b1b2aeba",
             measurementId: "G-8CF4080EYT"
         });
+        this.firestore = firebase.firestore();
+        this.googleProvider = new firebase.auth.GoogleAuthProvider();
+    }
+
+
+    getUserData = (id) => this.firestore.collection("users").doc(id).get();
+    getOrders = async () => {
+        const ref = this.firestore.collection("orders").doc(firebase.auth().currentUser.uid);
+        const snapshot = await ref.get();
+        if (snapshot.exists) {
+            return snapshot.data().orders;
+        }
+        return [];
+    }
+    getStocks = async () => {
+        const ref = this.firestore.collection("stocks").doc(firebase.auth().currentUser.uid);
+        const snapshot = await ref.get();
+        if (snapshot.exists) {
+            return snapshot.data().stocks;
+        }
+        return [];
     }
 }
+
+const firebaseInstance = new Firebase();
+export default firebaseInstance;
