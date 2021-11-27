8e1e1a267a7e478168e5efbef4f70ea94f98fe23
Théo <theo.gardet@epita.fr>
2021-11-27 12:15:41 +0100

    User: progress

--- restock/src/components/User.js
+++ restock/src/components/User.js
@@ -1,33 +1,45 @@
 import { Menu } from '@headlessui/react'
 
+
 function User( { user }) {
     return (
-      <Menu>
-        <Menu.Button className="flex m-3 p-2 mb-0 mt-0 float-right  border-b border-solid border-blueGray-100">User Name <img src="user_icon.png" alt="Icon"></img></Menu.Button>
-        <Menu.Items className="clear-both">
-          <Menu.Item className="m-3 p-2 mt-0 mb-0 float-right clear-both">
-            {({ active }) => (
-              <a
-                className={`${active ? 'bg-blue-500 text-white' : 'bg-white text-black'}`}
-                href=""
-              >
-                aAA
-              </a>
-            )}
-          </Menu.Item>
-          <Menu.Item className="m-3 p-2 mt-0 float-right clear-both">
-            {({ active }) => (
-              <a
-                className={`${active ? 'bg-blue-500 text-white' : 'bg-white text-black'}`}
-                href="/account-settings"
-              >
-                Log Out
-              </a>
-            )}
-          </Menu.Item>
-        </Menu.Items>
-      </Menu>
-    )
-  }
+        <div className="w-56 text-right">
+          <Menu as="div" className="relative inline-block text-left">
+            <div>
+              <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-black rounded-md hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                Options
+              </Menu.Button>
+            </div>
+            <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="m-1">
+                <Menu.Item>
+                {({ active } ) => (
+                    <button
+                    className={`${
+                        active ? 'bg-gray-200' : 'text-gray-900'
+                    } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                    >
+                    Settings
+                    </button>
+                )}
+                </Menu.Item>
+                <Menu.Item>
+                {({ active }) => (
+                    <button
+                    className={`${
+                        active ? 'bg-gray-200' : 'text-gray-900'
+                    } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                    >
+                    Log out
+                    </button>
+                )}
+                </Menu.Item>
+                </div>
+            </Menu.Items>
+          </Menu>
+        </div>
+      )
+    }
+    
 
 export default User;
